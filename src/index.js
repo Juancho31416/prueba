60e331949930f48029283dea656cc99c8de12dce
juan <paisaroloco@gmail.com>
2018-05-28 22:09:29 -0500

    commit prueba

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Login from "./containers/Login";
+
+
+
+ReactDOM.render(
+  <h1>Funciona</h1>,
+  document.getElementById('app'),
+);
+if (module.hot) {
+  module.hot.accept();
+}
